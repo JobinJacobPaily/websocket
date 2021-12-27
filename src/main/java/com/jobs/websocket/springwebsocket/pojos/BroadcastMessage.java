e2eca2947ce5157b69eff3d185501124aa9b6202
Jobin Jacob Paily <jobinjacobpaily@gmail.com>
2021-12-27 18:20:53 +0530

    Changes ---------------------- Pojo added

---
+++ src/main/java/com/jobs/websocket/springwebsocket/pojos/BroadcastMessage.java
@@ -0,0 +1,13 @@
+package com.jobs.websocket.springwebsocket.pojos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BroadcastMessage {
+
+    private String message;
+}
---
+++ src/main/java/com/jobs/websocket/springwebsocket/pojos/ReceivedMessage.java
@@ -0,0 +1,14 @@
+package com.jobs.websocket.springwebsocket.pojos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReceivedMessage {
+
+    private String message;
+
+}
