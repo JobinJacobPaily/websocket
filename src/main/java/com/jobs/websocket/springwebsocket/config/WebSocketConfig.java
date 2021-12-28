c80cc65daa2b50ae64330e25d0ac7fa2902a7072
Jobin Jacob Paily <jobinjacobpaily@gmail.com>
2021-12-28 17:26:35 +0530

    sheduled message

--- src/main/java/com/jobs/websocket/springwebsocket/config/WebSocketConfig.java
+++ src/main/java/com/jobs/websocket/springwebsocket/config/WebSocketConfig.java
@@ -2,10 +2,10 @@ package com.jobs.websocket.springwebsocket.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -20,4 +20,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/broker");
         registry.setApplicationDestinationPrefixes("/web");
     }
+
 }
--- src/main/java/com/jobs/websocket/springwebsocket/controller/WebSocketController.java
+++ src/main/java/com/jobs/websocket/springwebsocket/controller/WebSocketController.java
@@ -2,17 +2,37 @@ package com.jobs.websocket.springwebsocket.controller;
 
 import com.jobs.websocket.springwebsocket.pojos.BroadcastMessage;
 import com.jobs.websocket.springwebsocket.pojos.ReceivedMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+import java.util.Date;
+
+@EnableScheduling
 @Controller
 public class WebSocketController {
+
+    @Autowired
+    SimpMessagingTemplate simpMessagingTemplate;
 
     @MessageMapping("/send")
     @SendTo("/broker/receive")
 public BroadcastMessage recivedMessage(ReceivedMessage receivedMessage)  {
+
     return new BroadcastMessage(HtmlUtils.htmlEscape(receivedMessage.getMessage()));
+
+}
+
+@Scheduled(fixedRate = 5000)
+public void sheduledMessage() {
+        BroadcastMessage broadcastMessage = new BroadcastMessage();
+        String date = new Date().toString();
+        broadcastMessage.setMessage("Now the time is "+date);
+        simpMessagingTemplate.convertAndSend("/broker/receive",broadcastMessage);
 }
 }
