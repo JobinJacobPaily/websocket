6b4d153efffd31b0594dda26801b67eab5009399
Jobin Jacob Paily <jobinjacobpaily@gmail.com>
2021-12-31 17:30:53 +0530

    Changes --------------------- Cors config added

--- src/main/java/com/jobs/websocket/springwebsocket/config/WebSocketConfig.java
+++ src/main/java/com/jobs/websocket/springwebsocket/config/WebSocketConfig.java
@@ -12,7 +12,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket-example").withSockJS();
+        registry.addEndpoint("/websocket-example")
+                .setAllowedOrigins("http://localhost:8080")
+                .withSockJS();
     }
 
     @Override
